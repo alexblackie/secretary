688881a386c8a7118eefeec00e7a4418b01999a9
Alex Blackie <alex@alexblackie.com>
2016-11-15 21:00:37 -0800

    Add plug
    we'll need to accept HTTP requests

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Secretary.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger],
+    [applications: [:logger, :cowboy, :plug],
      mod: {Secretary, []}]
   end
 
@@ -28,6 +28,9 @@ defmodule Secretary.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:cowboy, "~> 1.0.0"},
+      {:plug, "~> 1.2.2"}
+    ]
   end
 end
