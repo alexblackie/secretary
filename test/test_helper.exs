4a261dfc442549142f9324a54403918af5510895
Alex Blackie <alex@alexblackie.com>
2016-11-19 12:14:28 -0800

    Fix path of vcr cassettes

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
-ExVCR.Config.cassette_library_dir("fixtures/vcr_cassettes")
+ExVCR.Config.cassette_library_dir("test/fixtures/vcr_cassettes")
 ExVCR.Config.filter_sensitive_data(Application.get_env(:secretary, :github_token), "<GITHUB_TOKEN>")
 
 ExUnit.start()
